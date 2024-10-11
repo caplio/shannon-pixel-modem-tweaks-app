40ecd9ab5e53c2e7f36b985a556f7987031e9780
caplio <github@snugplace.net>
2024-10-12 02:45:53 +0900

    Update NrMmwave.kt

--- app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/NrMmwave.kt
+++ app/src/main/java/dev/davwheat/shannonmodemtweaks/tweaks/nvitems/NrMmwave.kt
@@ -8,19 +8,19 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 class NrMmwave: NvItemTweak(), Parcelable {
   @IgnoredOnParcel
-  override val name = "Enable n257, n258, n260 and n261"
+  override val name = "Enable n257, n258, n260 and n261 & DL256QAM"
   @IgnoredOnParcel
   override val description = "Applies to both SIMs"
 
   override val nvItems: List<NvItem>
     get() =
         listOf(
-            NvItem(id = "UECAPA_NR_RF_BAND_FR2_BM", value = 49242.toNvItemHexString(2)),
+            NvItem(id = "UECAPA_NR_RF_BAND_FR2_BM", value = 50266.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_21_FBI", value = 10257.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_22_FBI", value = 10258.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_23_FBI", value = 10260.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_24_FBI", value = 10261.toNvItemHexString(2)),
-            NvItem(id = "UECAPA_NR_RF_BAND_FR2_BM_DS", value = 49242.toNvItemHexString(2)),
+            NvItem(id = "UECAPA_NR_RF_BAND_FR2_BM_DS", value = 50266.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_21_FBI_DS", value = 10257.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_22_FBI_DS", value = 10258.toNvItemHexString(2)),
             NvItem(id = "UECAPA_NR_RF_BAND_23_FBI_DS", value = 10260.toNvItemHexString(2)),
